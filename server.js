81f6608911e1e6a89ee61ce82fdeec03454cf465
MoidHasanBeig <mhbdgr8@gmail.com>
2020-07-11 00:46:54 +0530

    Add subdomains

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const subdomain = require('express-subdomain');
 const path = require('path');
 const app = express();
+const router = express.Router();
+
+app.use(subdomain('webcam', router));
 
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', (req, res) => {
-  return res.send('pong');
-});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+router.get('/users', function(req, res) {
+    res.json([
+        { name: "Brian" }
+    ]);
+});
+
 
 app.listen(8080);
