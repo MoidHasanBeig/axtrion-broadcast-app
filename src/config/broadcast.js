3ca85bcdcd102502ead04495c4e19b9d2422e5ef
MoidHasanBeig <mhbdgr8@gmail.com>
2020-07-11 17:11:40 +0530

    Production error fix

--- src/config/broadcast.js
+++ src/config/broadcast.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://www.reactapp.xyz";
+const ENDPOINT = "webcam.reactapp.xyz";
 
 function configBroadcast() {
   const peerConnections = {};
--- src/config/watch.js
+++ src/config/watch.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://www.reactapp.xyz";
+const ENDPOINT = "watchlive.reactapp.xyz";
 
 function configWatch() {
   let peerConnection;
