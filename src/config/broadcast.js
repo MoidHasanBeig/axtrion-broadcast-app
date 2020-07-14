9d7f8ca56393614ec0c5e5e6e137e305310ddecd
MoidHasanBeig <mhbdgr8@gmail.com>
2020-07-14 13:31:18 +0530

    Socket trial again

--- src/config/broadcast.js
+++ src/config/broadcast.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "reactapp.xyz";
+const ENDPOINT = "webcam.reactapp.xyz";
 
 function configBroadcast() {
   const peerConnections = {};
