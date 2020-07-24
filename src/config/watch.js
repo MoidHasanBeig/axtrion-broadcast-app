d06135fc3458182b41cc764e9da1c139f704e07c
MoidHasanBeig <MoidHasanBeig@github.com>
2020-07-24 12:30:27 +0530

    Local host to network host

--- src/config/watch.js
+++ src/config/watch.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://localhost:8080";
+const ENDPOINT = "webbcast.herokuapp.com";
 
 function configWatch(setIsConnected,inputValue) {
   let peerConnection;
